aa90a8397138e5127d4cacc323a5c67891cb4f1d
janiapurva <jani.apurva@yahoo.com>
2021-01-13 15:50:19 -0500

    adding file

--- client.js
+++ client.js
@@ -11,20 +11,128 @@ const connect = function() {
     port: 50542
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8');
-  
-  conn.on('data',(input) => {
-    console.log(input);
-  });
-  //notify when server connected
+   conn.setEncoding('utf8');
+  // stdin.on('data' , (key,conn) =>{
+  //   handleInput(key,conn);
+  // });
+
   conn.on('connect',() => {
     console.log(`Successfully connected to game server`);
-    conn.write('Name:apu');
+    //registring name
+    conn.write("Name: apu");
+
+
+    //command up
+    conn.write("Move: up");
+    
+    //setting interval
+    let counter;
+    setTimeout(() =>{
+      conn.write('Move: right');
+    },counter += 50);
+    setTimeout(() =>{
+      conn.write('Move: left');
+    },counter += 50);
+
+    setTimeout(() =>{
+      conn.write('Move: down');
+    },counter += 50);
+
+    setTimeout(() =>{
+      conn.write('Move: up');
+    },counter += 50);
+    setTimeout(() =>{
+      conn.write('Move: right');
+    },counter += 50);
+
+    
+
+
+
+    
+    //notify when server connected
+  
+
+    //command up
+    
+
   });
 
   
 
   return conn;
 };
+
+
+
+let handleInput = function(key,conn) {
+
+  if (key === "\u0077") {
+    conn.write("Move: up");
+  }
+};
+
+
+
+
+
+// const command = function() {
+  
+  
+  
+//   //notify when server connected
+//   conn.on('connect',() => {
+//     //command up
+//     conn.write("Move: up");
+//     //command down
+//     conn.write('Move: down');
+//     //command left
+//     conn.write('Move: left');
+//     //command right
+//     conn.write('Move: right');
+
+//     //setting interval
+//     let counter;
+//     setTimeout(() =>{
+//       conn.write('Move: right');
+//     },counter += 50);
+//     setTimeout(() =>{
+//       conn.write('Move: left');
+//     },counter += 50);
+
+//     setTimeout(() =>{
+//       conn.write('Move: down');
+//     },counter += 50);
+
+//     setTimeout(() =>{
+//       conn.write('Move: up');
+//     },counter += 50);
+//     set(() =>{
+//       conn.write('Move: right');
+//     },counter += 50);
+
+//     setInterval(() =>{
+//       conn.write('Move: up');
+//     },counter += 50);
+
+
+    
+
+//   });
+
+  
+
+//   return conn;
+// };
+
+
+
+
+
+
+
+
+
+
 
 module.exports = {connect};
