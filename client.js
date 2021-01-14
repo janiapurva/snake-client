7913aff72faeff9b93bd3a14a1bafe5de101942c
janiapurva <jani.apurva@yahoo.com>
2021-01-13 19:27:16 -0500

    adding changes

--- client.js
+++ client.js
@@ -1,17 +1,17 @@
-
+const {host,port} = require('./constants');
 const stdin = process.stdin;
-
 const { write } = require('fs');
+
 const net = require('net');
 
 
 const connect = function() {
   const conn = net.createConnection({
-    host: `135.23.222.131` ,
-    port: 50542
+    host,
+    port
   });
   // interpret incoming data as text
-   conn.setEncoding('utf8');
+  conn.setEncoding('utf8');
   // stdin.on('data' , (key,conn) =>{
   //   handleInput(key,conn);
   // });
---
+++ constants.js
@@ -0,0 +1,9 @@
+const host = `135.23.222.131`;
+const port = `50542`;
+
+
+
+module.exports = {
+  host,
+  port
+};
